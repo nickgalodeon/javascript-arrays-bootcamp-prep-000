9d14cf616b835e29a5f5fb39f9bfa9361881e390
Nick Lozenski <nicholas.lozenski@gmail.com>
2018-04-07 17:49:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,4 @@ const array = [1]
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
+function deconstructivelyAddElementToBeginningOfArray(array,element){return [array.unshift(element)]}
